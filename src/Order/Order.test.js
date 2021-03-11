468c77797d2bf4c31de1fe991bc099b2c6f7ae11
naysudes <syrbulova@gmail.com>
2021-03-11 20:59:21 +0300

    95%

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import {configure, shallow} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import Order from "./Order";
+import {getDate} from '../utils/getDate';
+import {fakeOrders} from "../data/fakeOrders";
+import toJson from "enzyme-to-json";
+
+
+jest.mock('../utils/getDate')
+configure({adapter: new Adapter()});
+
 
 describe('Order.js', () => {
-  it('some test', () => {
 
+    afterAll(() => {
+        getDate.mockReset();
+    });
+    beforeEach(() => {
+        getDate.mockClear();
+        getDate.mockReturnValue(123);
+    });
+
+    it('no params', () => {
+        const wrapper = shallow(<Order order={{}}/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+    
+    it('empty', () => {
+        const wrapper = shallow(<Order/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('only data', () => {
+        let order = Object.assign({}, fakeOrders[0]);
+        delete order.items;
+        delete order.shop;
+        const wrapper = shallow(<Order order={order}/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('data and items', () => {
+        const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('order without date', () => {
+    let order = Object.assign({}, fakeOrders[0]);
+    delete order.date;
+    const wrapper = shallow(<Order order={order}/>);
+    expect(wrapper.getElement()).toBeNull();
   });
-});
 
+});
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, getSortFunction, sortTypes, sortByDate, sortOrders} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -18,6 +18,71 @@ describe('sortByItemCount function', () => {
 
 		const result = sortByItemCount(order1, order2);
 
+		expect(result).toBe(0);
+	});
+
+	it('no items', () => {
+		const order3 = {noitems: "123"}
+		const result = sortByItemCount(order3, order3);
+
+		expect(result).toBe(0);
+	});
+
+		it('wrong type', () => {
+		const order3 = {items: "123"}
+		const result = sortByItemCount(order3, 1);
+
+		expect(result).toBe(0);
+	});
+});
+
+describe('sortType function', () => {
+	it('sortByDate', () => {
+		const result = getSortFunction(sortTypes.DATE);
+		expect(result).toBe(sortByDate);
+	});
+
+	it('sortByItemCount', () => {
+		const result = getSortFunction(sortTypes.COUNT);
+		expect(result).toBe(sortByItemCount);
+	});
+});
+
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('wrong type', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+		const result = sortByDate(order1, 1);
+		expect(result).toBe(0);
+	});
+
+	const order1 = {date: 5};
+	const order2 = {date: 10};
+
+	it('first order', () => {
+		const result = sortByDate(order2, order1);
+		expect(result).toBe(-1);
+	});
+
+	it('second order', () => {
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(1);
+	});
+
+	it('equals', () => {
+		const result = sortByDate(order1, order1);
+		expect(result).toBe(0);
+	});
+
+	it('no date', () => {
+		const order3 = {nodate: 1}
+		const result = sortByDate(order3, order1);
 		expect(result).toBe(0);
 	});
 });
